7b7938411253d52ba5e2cdc7a554863b8dfaee6f
Vuk Colovic <vukcolovic@gmail.com>
2021-01-22 21:51:08 +0100

    Set web server

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 )
 
 type person struct {
@@ -33,4 +34,16 @@ func main() {
 	}
 
 	fmt.Println("Back in to go struct: ", xp2)
+
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+
 }
