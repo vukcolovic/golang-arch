49c07b4e0f755c5308be8a7703dec2a0247566c5
Vuk Colovic <vukcolovic@gmail.com>
2021-01-23 18:10:26 +0100

    Add claims

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/vukcolovic/golang-arch
 go 1.14
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 	golang.org/x/text v0.3.5 // indirect
 )
--- main.go
+++ main.go
@@ -6,7 +6,26 @@ import (
 	"fmt"
 	"golang.org/x/crypto/bcrypt"
 	"log"
+	"github.com/dgrijalva/jwt-go"
+	"time"
 )
+
+type UserClaims struct {
+	jwt.StandardClaims
+	SessionID int64
+}
+
+func(u *UserClaims) Valid() error {
+	if !u.VerifyExpiresAt(time.Now().Unix(), true) {
+		return fmt.Errorf("Token has expired")
+	}
+
+	if u.SessionID == 0 {
+		return fmt.Errorf("Invalid session ID")
+	}
+
+	return nil
+}
 
 var key = []byte{}
 
