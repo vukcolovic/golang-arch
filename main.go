504e0c8f3c428a285ac7a0872eab4d001855000d
Vuk Colovic <vukcolovic@gmail.com>
2021-01-27 23:44:45 +0100

    Exercise2

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ var db = map[string][]byte{}
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/register", register)
-	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/login", login)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -29,10 +29,17 @@ func index(w http.ResponseWriter, r *http.Request) {
 		</head>
 		<body>
 			<p>` + errMsg + `</p>
+			<h1>REGISTER</h1>
 			<form action="/register" method="POST">	
 				<input type="text" name="name"/>
 				<input type="password" name="password"/>
-				<input type="submit" name="Submit"/>
+				<input type="submit" name="Register"/>
+			</form>
+			<h1>LOG IN</h1>
+			<form action="/login" method="POST">	
+				<input type="text" name="name"/>
+				<input type="password" name="password"/>
+				<input type="submit" name="Login"/>
 			</form>
 		</body>
 	</html>`
@@ -67,25 +74,36 @@ func register(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/hello?name=" + name, http.StatusSeeOther)
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	names, ok := r.URL.Query()["name"]
-	name := "anonimus"
-	if ok {
-		name = names[0]
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		errorMsg := url.QueryEscape("your method was not post")
+		http.Redirect(w, r, "/?errorMsg=" + errorMsg, http.StatusSeeOther)
+		return
 	}
 
-	html := `<!DOCTYPE html>
-	<html lang="en"
-		<head>
-			<meta charset="UTF-8"> 
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<meta http-equiv="X-UA-Compatible" content="ie=edge">
-			<title>HMAC Example</title>
-		</head>
-		<body>
-			<p>Hello ` + name + `</p>
-		</body>
-	</html>`
+	name := r.FormValue("name")
+	password := r.FormValue("password")
 
-	io.WriteString(w, html)
+	if name == "" || password == "" {
+		errorMsg := url.QueryEscape("name or password is empty")
+		http.Redirect(w, r, "/?errorMsg=" + errorMsg, http.StatusSeeOther)
+		return
+	}
+
+	if _, ok := db[name]; !ok {
+		errorMsg := url.QueryEscape("your name and password don't match")
+		http.Redirect(w, r, "/?errorMsg=" + errorMsg, http.StatusSeeOther)
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(db[name]), []byte(password))
+	if err != nil {
+		errorMsg := url.QueryEscape("wrong password")
+		http.Redirect(w, r, "/?errorMsg=" + errorMsg, http.StatusSeeOther)
+		return
+	}
+
+	errorMsg := url.QueryEscape("you logged in " + name)
+	http.Redirect(w, r, "?errorMsg=" + errorMsg, http.StatusSeeOther)
 }
+
